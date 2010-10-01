f0b675e1d8e008831423792161c774cfcaeedda5
James Turk <james.p.turk@gmail.com>
2010-09-30 23:57:50 -0400

    discovery of AZ XML

--- fiftystates/scrape/az/__init__.py
+++ fiftystates/scrape/az/__init__.py
@@ -1,5 +1,7 @@
 import datetime
 
+# session information available from http://azleg.gov/xml/sessions.asp
+
 metadata = dict(
     name='Arizona',
     abbreviation='az',
@@ -28,26 +30,46 @@ metadata = dict(
          ]
         },
     ],
-    sesssion_details={
+    session_details={
         'Forty-ninth Legislature - First Regular Session':
-            {'type': 'primary', 'session_id': 87},
+            {'type': 'primary', 'session_id': 87,
+             'start_date': datetime.date(2009,1,12),
+             'end_date': datetime.date(2009,7,1)},
         'Forty-ninth Legislature - First Special Session':
-            {'type': 'special', 'session_id': 89},
+            {'type': 'special', 'session_id': 89,
+             'start_date': datetime.date(2009,1,28),
+             'end_date': datetime.date(2009,1,31)},
         'Forty-ninth Legislature - Second Special Session':
-            {'type': 'special', 'session_id': 90},
+            {'type': 'special', 'session_id': 90,
+             'start_date': datetime.date(2009,5,21),
+             'end_date': datetime.date(2009,5,27)},
         'Forty-ninth Legislature - Third Special Session':
-            {'type': 'special', 'session_id': 91},
+            {'type': 'special', 'session_id': 91,
+             'start_date': datetime.date(2009,7,6),
+             'end_date': datetime.date(2009,8,25)},
         'Forty-ninth Legislature - Fourth Special Session':
-            {'type': 'special', 'session_id': 92},
+            {'type': 'special', 'session_id': 92,
+             'start_date': datetime.date(2009,11,17),
+             'end_date': datetime.date(2009,11,23)},
         'Forty-ninth Legislature - Fifth Special Session':
-            {'type': 'special', 'session_id': 94},
+            {'type': 'special', 'session_id': 94,
+             'start_date': datetime.date(2009,12,17),
+             'end_date': datetime.date(2009,12,19)},
         'Forty-ninth Legislature - Sixth Special Session':
-            {'type': 'special', 'session_id': 95},
+            {'type': 'special', 'session_id': 95,
+             'start_date': datetime.date(2010,2,1),
+             'end_date': datetime.date(2010,2,11)},
         'Forty-ninth Legislature - Seventh Special Session':
-            {'type': 'special', 'session_id': 96},
+            {'type': 'special', 'session_id': 96,
+             'start_date': datetime.date(2010,3,8),
+             'end_date': datetime.date(2010,3,16)},
         'Forty-ninth Legislature - Eighth Special Session':
-            {'type': 'special', 'session_id': 101},
+            {'type': 'special', 'session_id': 101,
+             'start_date': datetime.date(2010,3,29),
+             'end_date': datetime.date(2010,4,1)},
         'Forty-ninth Legislature - Second Regular Session':
-            {'type': 'primary', 'session_id': 93},
+            {'type': 'primary', 'session_id': 93,
+             'start_date': datetime.date(2010,1,11),
+             'end_date': datetime.date(2010,4,29)},
     }
 )
--- fiftystates/scrape/az/bills.py
+++ fiftystates/scrape/az/bills.py
@@ -1,7 +1,7 @@
 import datetime
 import re
 
-import lxml.html
+import lxml.etree
 
 from fiftystates.scrape.bills import BillScraper, Bill
 
@@ -9,15 +9,27 @@ class AZBillScraper(BillScraper):
 
     state = 'az'
 
+    def get_session_id(self, session):
+        return self.metadata['session_details'][session]['session_id']
+
+    def scrape_bill(self, chamber, session, bill_id):
+        bill_url = 'http://azleg.gov/xml/billinfo.asp?Session=%s&Bill=%s' % (
+            self.get_session_id(session), bill_id)
+
+        with self.urlopen(bill_url) as data:
+            pass
+
+
     def scrape(self, chamber, session):
-        chamber = {'lower': 'allhouse', 'upper': 'allsenate'}[chamber]
-        session_id = self.metadata['session_details'][session]['session_id']
+        session_id = self.get_session_id(session)
+        chamber_letter = {'lower': 'H', 'upper': 'S'}[chamber]
 
-        url = 'http://www.azleg.gov/Bills.asp?format=print&view=%s&Session_ID=%s'
-        url = url % (chamber, session_id)
-
-        print url
+        url = 'http://azleg.gov/xml/bills.asp?session=' + session_id
 
         with self.urlopen(url) as data:
-            pass
+            doc = lxml.etree.fromstring(data)
+            for bill in doc.xpath('//bill'):
+                number, old_title, cur_title = bill.xpath('*/text()')
+                if number[0] == chamber_letter:
+                    self.scrape_bill(chamber, session, number)
 
